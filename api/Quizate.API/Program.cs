53ddebd34bc07c6e9a290647482a4397803a487e
Onur Güdeloğlu <onurgudeloglu@gmail.com>
2026-02-14 15:51:02 +0300

    feat(api): initializing project

---
+++ api/Quizate.API/Program.cs
@@ -0,0 +1,27 @@
+namespace Quizate.API;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        var builder = WebApplication.CreateBuilder(args);
+
+
+        builder.Services.AddControllers();
+
+        builder.Services.AddAuthorization();
+
+
+        var app = builder.Build();
+
+
+        app.UseHttpsRedirection();
+
+        app.UseAuthorization();
+
+        app.MapControllers();
+
+
+        app.Run();
+    }
+}
